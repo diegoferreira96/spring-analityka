b72a6ea19048ca72074e17e2338933157b9eb82f
Diego Ferreira <diegoferreira_devweb@hotmail.com>
2026-06-17 21:05:36 -0300

    Inicio do Projeto

---
+++ src/main/java/br/com/backend/spring_analityka/SpringAnalitykaApplication.java
@@ -0,0 +1,13 @@
+package br.com.backend.spring_analityka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnalitykaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnalitykaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/backend/spring_analityka/SpringAnalitykaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.spring_analityka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnalitykaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
